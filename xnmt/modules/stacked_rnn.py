01524920ec302c3ee46f89077e3f808e8f50fe0b
zjz17 <1428364105@qq.com>
2018-04-29 10:35:22 +0800

    add stacked rnn module

---
+++ xnmt/modules/stacked_rnn.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+
+class StackedLSTM(nn.Module):
+    """
+    Implementation of stacked LSTM borrowed from OpenNMT-py
+    # https://github.com/OpenNMT/OpenNMT-py/blob/master/onmt/modules/StackedRNN.py
+    Needed for the decoder if input feeding is required
+    """
+    def __init__(self, num_layers, input_size, rnn_size, dropout):
+        super(StackedLSTM, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.num_layers = num_layers
+        self.layers = nn.ModuleList()
+
+        for i in range(num_layers):
+            self.layers.append(nn.LSTMCell(input_size, rnn_size))
+            input_size = rnn_size
+
+    def forward(self, input, hidden):
+        h_0, c_0 = hidden
+        h_1, c_1 = [], []
+        for i, layer in enumerate(self.layers):
+            h_1_i, c_1_i = layers(input, (h_0[i], c_0[i]))
+            input = h_1_i
+            if i + 1 != self.num_layers:
+                input = self.dropout(input)
+            h_1 += [h_1_i]
+            c_1 += [c_1_i]
+
+        h_1 = torch.stack(h_1)
+        c_1 = torch.stack(c_1)
+        
+        return input, (h_1, c_1)
+
+class StackedGRU(nn.Module):
+
+    def __init__(self, num_layers, input_size, rnn_size, dropout):
+        super(StackedGRU, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.num_layers = num_layers
+        self.layers = nn.ModuleList()
+
+        for i in range(num_layers):
+            self.layers.append(nn.GRUCell(input_size, rnn_size))
+            input_size = rnn_size
+
+    def forward(self, input, hidden):
+        h_1 = []
+        for i, layer in enumerate(self.layers):
+            h_1_i = layer(input, hidden[i])
+            input = h_1_i 
+            if i + 1 != self.num_layers:
+                input = self.dropout(input)
+            h_1 += [h_1_i]
+
+        h_1 = torch.stack(h_1)
+        return input, h_1
