07ebf9e04d1af8f2d6ee7cd8884a30d0d4791ed7
zjz17 <1428364105@qq.com>
2018-05-26 09:37:33 +0800

    fix rl trainer errors and speed up

--- xnmt/trainer/rl_trainer.py
+++ xnmt/trainer/rl_trainer.py
@@ -8,7 +8,7 @@ from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction, sentence_b
 import torch
 import torch.nn as nn
 
-from xnmt.utils import drop_chkpt, load_chkpt, make_logger, Statistics
+from xnmt.utils import drop_chkpt, load_chkpt, make_logger, Statistics, sequence_mask
 from xnmt.io import Constants
 
 
@@ -24,7 +24,7 @@ class RLCriterion(object):
     Reference paper:
         * Simple Statistical Gradient-Following Algorithms for Connectionist Reinforcement Learning
     """
-    def __init__(self, reward_type='bleu'):
+    def __init__(self, reward_type, ce_criterion):
         self.reward_type = reward_type
         if reward_type == 'bleu':
             self.get_reward = self.get_bleu
@@ -36,10 +36,9 @@ class RLCriterion(object):
             raise Exception('Unsupported reward type')
         # just a single scalar to keep the average of the history rewards 
         # if baseline is not given 
-        self.baseline = 0
-        self.ratio = 0.9
+        self.ce_criterion = ce_criterion
 
-    def get_bleu(self, target, sample):
+    def get_bleu(self, target, sample, length):
         """Compute reward given the reference target and sampled sentence.
 
         Args:
@@ -47,18 +46,22 @@ class RLCriterion(object):
                 * batch_size * target_len
             sample (tensor): just like above, but is the sampled sentence.
                 * batch_size * sample_len
+            length (torch.LongTensor): batch_size, the effective length of each sample in a batch
         Returns:
             rewards (tensor): tensor containing reward for each sampled unit
                 * batch_size * sample_len
         """
         
+        batch_size, sample_len = sample.size()
         target_list = target.cpu().tolist()
         sample_list = sample.cpu().tolist()
-        batch_size, sample_len = sample.size()
-        
+        sample_length = length.cpu().tolist()
+        target_length = [lis.index(Constants.EOS)+1 for lis in target_list]
         rewards = []
         for i in range(batch_size):
-            score = sentence_bleu([target_list[i]], sample_list[i],
+            tgt = target_list[i][:target_length[i]]
+            spe = sample_list[i][:sample_length[i]]
+            score = sentence_bleu([tgt], spe,
                 weights = (0.25, 0.25, 0.25, 0.25),
                 smoothing_function = SmoothingFunction().method2
             )
@@ -67,11 +70,11 @@ class RLCriterion(object):
         rewards = rewards.view(-1, 1).repeat(1, sample_len) 
         return rewards
 
-    def get_ce_reward(self, target, sample):
+    def get_ce_reward(self, target, sample, length):
         """Compute reward given the reference target and sampled sentence. 
 
         ======================================================
-        Note: This is not compatible with the REINFORCE theory.
+        Note: This may not be compatible with the REINFORCE theory.
         ======================================================
 
         Performs just like cross entropy which gives word-level reward.
@@ -81,6 +84,7 @@ class RLCriterion(object):
                 * batch_size * target_len
             sample (tensor): just like above, but is the sampled sentence.
                 * batch_size * sample_len
+            length (torch.LongTensor): batch_size, the effective length of each sample in a batch
         Returns:
             rewards (tensor): tensor containing reward for each sampled unit
                 * batch_size * sample_len
@@ -91,12 +95,23 @@ class RLCriterion(object):
         if target_len >= sample_len:
             target = target[:, :sample_len]
         else:
-            tmp = torch.ones((batch_size, sample_len-target_len)).type_as(sample)
+            tmp = torch.ones((batch_size, sample_len-target_len)).type_as(sample) * -1
             target = torch.cat([target, tmp], dim=1)
         rewards = torch.eq(sample, target).type(torch.float).to(sample.device)
+        mask = sequence_mask(length, sample.size(1))
+        rewards = rewards * mask.float()
+        """
+        new_rewards = torch.ones((rewards.size(0), rewards.size(1))).type_as(rewards)
+        for i in range(batch_size):
+            for j in range(sample_len):
+                if rewards[i, j].item() == 0.:
+                    new_rewards[i, j:] = 0.
+                    break
+        rewards = new_rewards
+        """
         return rewards
 
-    def get_acc(self, target, sample):
+    def get_acc(self, target, sample, length):
         """Compute reward given the reference target and sampled sentence. 
 
         Performs just like cross entropy which gives word-level reward.
@@ -106,6 +121,7 @@ class RLCriterion(object):
                 * batch_size * target_len
             sample (tensor): just like above, but is the sampled sentence.
                 * batch_size * sample_len
+            length (torch.LongTensor): batch_size, the effective length of each sample in a batch
         Returns:
             rewards (tensor): tensor containing reward for each sampled unit
                 * batch_size * sample_len
@@ -116,15 +132,19 @@ class RLCriterion(object):
         if target_len >= sample_len:
             target = target[:, :sample_len]
         else:
-            tmp = torch.ones((batch_size, sample_len-target_len)).type_as(sample)
+            tmp = torch.ones((batch_size, sample_len-target_len)).type_as(sample) * -1
             target = torch.cat([target, tmp], dim=1)
         rewards = torch.eq(sample, target).type(torch.float).to(sample.device)
-        rewards = rewards.sum(dim=1, keepdim=True) / rewards.size(1)
+        mask = sequence_mask(length, sample.size(1))
+        rewards = rewards * mask.float()
+        rewards = rewards.sum(dim=1, keepdim=True) / length.view(-1, 1).float()
         rewards = rewards.repeat(1, sample_len)
         return rewards
 
     def get_sample_unit(self, probs, sample):
         """Get sampled unit from probs which need to backprop according to the sample
+
+        Note: this process is the same as cross entropy, and can be implemented by NLLLoss.
 
         Args:
             probs (tensor): probs that output samples
@@ -136,6 +156,8 @@ class RLCriterion(object):
                 similar to the loss units where gradients begin to backprop.
                 * batch_size * sample_len
         """
+
+        """
         batch_size = probs.size(0)
         sample_len = probs.size(1)
         indexes = [[], [], []]
@@ -145,32 +167,51 @@ class RLCriterion(object):
                 indexes[1].append(j)
                 indexes[2].append(sample[i][j].item())
         units = probs[indexes].view(batch_size, sample_len)
+        """
+        batch_size = probs.size(0)
+        units = self.ce_criterion(probs.view(-1, probs.size(-1)), sample.view(-1))
+        units = - units.view(batch_size, -1)
         return units
 
-    def __call__(self, probs, target, sample, baseline=None):
+
+    def __call__(self, probs, target, sample, length, baseline=None):
         """
         Args:
             probs (torch.Tensor): batch_size * uncertain_len * vocab_size
             target (torch.LongTensor): batch_size * seq_len
             sample (torch.LongTensor): batch_size * uncertain_len
+            length (torch.LongTensor): batch_size, the effective length of each sample in a batch
             baseline (torch.Tensor): batch_size * uncertain_len
+
+        Consideration about mask:
+            * Problem: For sequence generation task, the sample of each example should be of 
+            variable-length, but all examples in a batch must be the same length. So
+            some samples may continue to sample even if they have output <eos> before.
+            * Solution: mask all the samples after the first <eos>.
         """
+
         # target = target[:, :-1]  # need not to eliminate EOS
         batch_size = probs.size()[0]
+
         units = self.get_sample_unit(probs, sample)
-        rewards = self.get_reward(target, sample)
+
+        rewards = self.get_reward(target, sample, length)
+
+        mask = sequence_mask(length, sample.size(1))
+
+
         reward_avg = rewards.sum().item()  / rewards.numel()
         if baseline is not None:
-            loss_b = (rewards - baseline).pow(2)
+            loss_b = (rewards - baseline).pow(2) * mask.float()
             loss_sum_b = torch.sum(loss_b) / batch_size
             rewards = rewards - baseline.detach()
-        
 
         #non_mask = 1 -  torch.eq(sample.data, Constants.PAD)
         #non_mask = non_mask.float()
         
         #loss = -non_mask * (rewards - self.baseline) * loss_units
-        loss = - rewards * units
+        #loss = - rewards * units
+        loss = - rewards * units * mask.float()
         loss_sum = torch.sum(loss) / batch_size
         if baseline is not None:
             loss_sum += loss_sum_b 
@@ -190,11 +231,11 @@ class RLTrainer(object):
         
     """
 
-    def __init__(self, model, reward_type, valid_criterion, optimizer, print_every, cuda=True):
+    def __init__(self, model, reward_type, ce_criterion, optimizer, print_every, cuda=True):
         self.cuda = cuda
         self.model = model
-        self.train_criterion = RLCriterion(reward_type)
-        self.valid_criterion = valid_criterion
+        self.train_criterion = RLCriterion(reward_type, ce_criterion)
+        self.valid_criterion = ce_criterion
         self.optimizer = optimizer
         self.print_every = print_every
         self.start_epoch = 1
@@ -246,9 +287,13 @@ class RLTrainer(object):
         samples = []
         log_probs = []
         baselines = []
+
+        sample_length = target.size(1)
+        lengths = torch.ones(batch_size).type_as(y_t)
+        before_eos = torch.ones(batch_size).type_as(y_t).byte()
+
+        sample_length = min(sample_length, self.max_sample_length)
         t = 0
-        sample_length = target.size(1) + 1
-        #while t < self.sample_length:
         while t < sample_length:
             t += 1
             
@@ -263,6 +308,10 @@ class RLTrainer(object):
             y_t = prob_t.multinomial(1)
             y_t = y_t.detach()  
             samples.append(y_t)  # batch_size * 1
+
+            # eos judgement
+            before_eos = torch.ne(y_t.view(-1), Constants.EOS) & before_eos
+            lengths += before_eos.long()
             
             # baseline calculation
             b_t_h = state.hidden[0].detach()[-1,:,:].squeeze(0)
@@ -276,7 +325,8 @@ class RLTrainer(object):
         samples = torch.cat(samples, dim=1)
         baseline = torch.cat(baselines, dim=1)
         # post processing
-        loss, reward = self.train_criterion(log_probs, target, samples, baseline)
+        #loss, reward = self.train_criterion(log_probs, target, samples, lengths, baseline)
+        loss, reward = self.train_criterion(log_probs, target, samples, lengths)
         return loss, reward
 
     def train_on_epoch(self, data_iter, epoch):
@@ -325,7 +375,7 @@ class RLTrainer(object):
             loss = self.valid_criterion(probs, target)
             
             # statistics
-            loss_data = loss.data.clone()
+            loss_data = torch.sum(loss).data.clone()
             batch_stat = self.get_stats(loss_data, probs.data, target.data)
             stats.update(batch_stat)
 
@@ -354,7 +404,7 @@ class RLTrainer(object):
             self.start_epoch += 1
 
         self.sample_size = sample_size
-        self.sample_length = sample_length
+        self.max_sample_length = sample_length
 
         for epoch in range(self.start_epoch, epochs+1):
             _ = self.train_on_epoch(train_data, epoch)
